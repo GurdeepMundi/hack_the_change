27f8df8c2f266c3e10f4d39a36709602a96e0f72
Gurdeep <gurdeepmundi10@gmail.com>
2022-11-20 12:37:10 -0700

    Pushing final update

--- trevile/src/App.js
+++ trevile/src/App.js
@@ -1,16 +1,128 @@
-import logo from './logo.svg';
+import React, { useState, useRef } from "react";
 import './App.css';
 import Button from 'react-bootstrap';
 import map from './images/map.png'
+import Logo from './images/logo.png'
 import PopUp from './PopUp'
-
+/*
 function App() {
   return (
     <div className="App">
       <img src={map} id="map" height={600} width={1100} />
-      <PopUp></PopUp>
+      
     </div>
   );
 }
+*/
+// export default App;
 
-export default App;
+
+import useSwr from "swr";
+import GoogleMapReact from "google-map-react";
+import useSupercluster from "use-supercluster";
+
+const fetcher = (...args) => fetch(...args).then(response => response.json());
+
+const Marker = ({ children }) => children;
+
+export default function App() {
+  const mapRef = useRef();
+  const [bounds, setBounds] = useState(null);
+  const [zoom, setZoom] = useState(10);
+
+  const url =
+    "https://data.police.uk/api/crimes-street/all-crime?lat=52.629729&lng=-1.131592&date=2019-10";
+  const { data, error } = useSwr(url, { fetcher });
+  const crimes = data && !error ? data.slice(0, 2000) : [];
+  const points = crimes.map(crime => ({
+    type: "Feature",
+    properties: { cluster: false, crimeId: crime.id, category: crime.category },
+    geometry: {
+      type: "Point",
+      coordinates: [
+        parseFloat(crime.location.longitude),
+        parseFloat(crime.location.latitude)
+      ]
+    }
+  }));
+
+  const { clusters, supercluster } = useSupercluster({
+    points,
+    bounds,
+    zoom,
+    options: { radius: 75, maxZoom: 20 }
+  });
+
+  return (
+    <div style={{ height: "90vh", width: "90%", margin:"0% 2%" }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "AIzaSyD9FBTj2odKmESaYWliBrQqr9IThvoLx0M" }}
+        defaultCenter={{ lat: 52.6376, lng: -1.135171 }}
+        defaultZoom={10}
+        yesIWantToUseGoogleMapApiInternals
+        onGoogleApiLoaded={({ map }) => {
+          mapRef.current = map;
+        }}
+        onChange={({ zoom, bounds }) => {
+          setZoom(zoom);
+          setBounds([
+            bounds.nw.lng,
+            bounds.se.lat,
+            bounds.se.lng,
+            bounds.nw.lat
+          ]);
+        }}
+      >
+        {clusters.map(cluster => {
+          const [longitude, latitude] = cluster.geometry.coordinates;
+          const {
+            cluster: isCluster,
+            point_count: pointCount
+          } = cluster.properties;
+
+          if (isCluster) {
+            return (
+              <Marker
+                key={`cluster-${cluster.id}`}
+                lat={latitude}
+                lng={longitude}
+              >
+                <div
+                  className="cluster-marker"
+                  style={{
+                    width: `${10 + (pointCount / points.length) * 20}px`,
+                    height: `${10 + (pointCount / points.length) * 20}px`
+                  }}
+                  onClick={() => {
+                    const expansionZoom = Math.min(
+                      supercluster.getClusterExpansionZoom(cluster.id),
+                      20
+                    );
+                    mapRef.current.setZoom(expansionZoom);
+                    mapRef.current.panTo({ lat: latitude, lng: longitude });
+                  }}
+                >
+                  {pointCount}
+                </div>
+              </Marker>
+            );
+          }
+
+          return (
+            <Marker
+              key={`crime-${cluster.properties.crimeId}`}
+              lat={latitude}
+              lng={longitude}
+            >
+              <button className="crime-marker">
+                <img src="/images/locationIcon.png" alt="crime location" />
+              </button>
+            </Marker>
+          );
+        })}
+      </GoogleMapReact>
+      <PopUp id="popup"></PopUp>
+    </div>
+
+  );
+}
